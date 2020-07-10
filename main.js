142413f344f87276f5f0c0b5fabc0cabc3ed9d3b
Juan <juan9-0-9@hotmail.com>
2020-07-10 17:24:14 -0300

    Crear Java Script and CSS

---
+++ main.js
@@ -0,0 +1,19 @@
+(function(){
+	// Variables
+	var lista = document.getElementById("lista"),
+		tareaInput = document.getElementById("tareaInput"),
+        btnNuevaTarea = document.getElementById("btn-buttom");
+        
+    //Funciones
+
+    var agregarTarea = function(){};
+
+    //eventos
+
+    //Agregar Tarea 
+
+     btnNuevaTarea.addEventListener("click", agregarTarea);
+
+
+    //comprobar Input
+    
